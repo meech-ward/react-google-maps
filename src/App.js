b7c40850691e7dae7d944d5edbebaf04a0504771
Sam Meech-Ward <sam@meech-ward.com>
2021-11-23 23:04:44 -0800

    🤗

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+
 import './App.css';
 
+import Map from './Map'
+
+const bcit = {
+  latitude: 49.28357705407476,
+  longitude: -123.11524407215198
+}
+
 function App() {
+  const [destination, setDestination] = useState(null)
+
+  function updateDesition() {
+    setDestination(bcit)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={updateDesition}>Go!</button>
+      <Map destination={destination}></Map>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Map.js
@@ -0,0 +1,90 @@
+import {useState, useEffect} from 'react'
+import GoogleMapReact from "google-map-react"
+import { geolocated } from "react-geolocated"
+import styled from "styled-components"
+
+
+const MapCont = styled.div`
+  height: 65vh;
+  min-width: 300px;
+  width: 80%;
+  margin: auto;
+  border: solid 5px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 1px 4px 5px 4px rgba(0, 0, 0, 0.3);
+  z-index: 0;
+`
+
+let directionsService, directionsRenderer, maps, map
+
+function Map({ coords, destination }) {
+
+  const [center, setCenter] = useState({})
+
+  useEffect(() => {
+    if (coords && destination && maps) {
+      calcRoute()
+      return 
+    }
+    if (coords) {
+      setCenter({
+        lat: coords?.latitude,
+        lng: coords?.longitude
+      })
+    }
+  }, [coords, destination])
+
+  function calcRoute() {
+    console.log("calculate the route")
+
+    const start = new maps.LatLng(coords.latitude, coords.longitude)
+    const end = new maps.LatLng(destination.latitude, destination.longitude)
+
+    const request = {
+      origin: start,
+      destination: end,
+      travelMode: "TRANSIT"
+    }
+
+    directionsService.route(request, function (result, status) {
+      if (status == "OK") {
+        directionsRenderer.setDirections(result)
+      }
+    })
+  }
+
+
+  function handleGoogleMapApi(details) {
+    maps = details.maps
+    map = details.map
+
+    directionsService = new maps.DirectionsService()
+    directionsRenderer = new maps.DirectionsRenderer()
+    directionsRenderer.setMap(map)
+  }
+
+  return (
+    <MapCont>
+      {coords ? (
+        <GoogleMapReact
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={handleGoogleMapApi}
+          bootstrapURLKeys={{ key: process.env.GOOGLE_API_KEY }}
+          center={center}
+          defaultZoom={15}
+        ></GoogleMapReact>
+      ) : (
+        <div>Getting Location...</div>
+      )}
+    </MapCont>
+  )
+}
+
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false
+  },
+  userDecisionTimeout: 5000,
+  watchPosition: true
+})(Map)
